330c612a81650d401f32e877a8829380c8d87a09
Markus Meissner <markus.meissner@meissner.IT>
2024-06-14 08:57:44 +0200

    Improved FreeBSD support

--- templates/mit-get-http-header.sh
+++ templates/mit-get-http-header.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/env sh
+#!/bin/sh
 #
 # Distributed via ansible - mit.zabbix-server.common
 #
-# v2016-08-16
+# v2024-05-14
+
+# FreeBSD needs PATH set
+export PATH=/bin:/usr/bin:/usr/local/bin
 
 curl -sS --head $1|grep "$2: "|awk '{print $2;}'
 
